7bb1e94ecf95c56a6e593f36ca9fa6f867437a5f
CliffordKasera <ckasera6486@gmail.com>
2022-05-31 22:19:14 +0000

    prints a string in uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def upper(i):
+    if ord(i) >= 97 and ord(i) <= 122:
+        return (ord(i) - 32)
+    else:
+        return ord(i)
+
+def uppercase(c):
+    empty_string = ""
+    for i in c:
+        empty_string += "%c" % upper(i)
+    print("{:s}".format(empty_string))
