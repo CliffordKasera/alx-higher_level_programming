61be4540cd21fd40c2f9daa934ddffc51f64ead9
CliffordKasera <ckasera6486@gmail.com>
2022-06-20 17:52:35 +0000

    fix bugs

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception():
-    try:
-        num = "Test error"
-        num[3] = "ok"
-    except TypeError:
-        print("Exception raised")
-        raise
+    raise TypeError
